91709a79f56223d733794516d9c39b0307c27d85
phlip <git@mrphlip.com>
2025-11-15 11:04:16 +1100

    Rearrange the tracks of 09, to split the PSG across two channels

--- 09-gen.py
+++ 09-gen.py
@@ -14,59 +14,69 @@ def load_midi():
 	return events
 
 def gen_notes():
-	notes = []
+	notes = [], []
+	ch_map = {6:0, 7:1, 8:0}
 
 	wheel = {6:0, 7:0, 8:0}
-	cur_ch = None
+	cur_ch = [None, None]
 
-	def end_note(time):
-		if notes:
-			notes[-1][1] = round(time / SCALE)
-	def add_note(time, note, wheel):
-		end_note(time)
+	def end_note(ch, time):
+		if notes[ch] and notes[ch][-1][1] is None:
+			notes[ch][-1][1] = round(time / SCALE)
+	def add_note(ch, time, note, wheel):
+		end_note(ch, time)
 		if wheel < 0 and note is not None:
 			wheel += 256
 			note -= 1
-		notes.append([round(time / SCALE), None, note, wheel])
+		notes[ch].append([round(time / SCALE), None, note, wheel])
+
+	add_note(0, 0, None, 0)
+	add_note(1, 0, None, 0)
 
 	for ev in load_midi():
 		if isinstance(ev.event, midifile.NoteOn):
-			add_note(ev.time, ev.event.key, wheel[ev.event.channel])
-			cur_ch = ev.event.channel
+			ch = ch_map[ev.event.channel]
+			add_note(ch, ev.time, ev.event.key, wheel[ev.event.channel])
+			cur_ch[ch] = ev.event.channel
 		elif isinstance(ev.event, midifile.NoteOff):
-			if ev.event.channel == cur_ch:
-				add_note(ev.time, None, wheel[ev.event.channel])
-				cur_ch = None
+			ch = ch_map[ev.event.channel]
+			if ev.event.channel == cur_ch[ch]:
+				add_note(ch, ev.time, None, wheel[ev.event.channel])
+				cur_ch[ch] = None
 		elif isinstance(ev.event, midifile.Wheel):
 			wheel[ev.event.channel] = round((ev.event.wheel - 8192) / 8192 * 256)
 		elif isinstance(ev.event, midifile.MetaEvent) and ev.event.event == midifile.Events.END_OF_TRACK:
-			end_note(ev.time)
+			end_note(0, ev.time)
+			end_note(1, ev.time)
 			break
 	return notes
 
-def split_barlines():
-	for startts, endts, note, wheel in gen_notes():
+def split_barlines(notes):
+	for startts, endts, note, wheel in notes:
 		if endts // BARLINES > startts // BARLINES:
 			yield startts, (endts // BARLINES) * BARLINES, note, wheel, False
 			yield (endts // BARLINES) * BARLINES, endts, None if note is None else -1, wheel, True
 		else:
 			yield startts, endts, note, wheel, False
 
-def filter_notes():
+def filter_notes(notes):
 	carry_newline = True
-	for startts, endts, note, wheel, newline in split_barlines():
+	for startts, endts, note, wheel, newline in notes:
 		carry_newline = carry_newline or newline
 		if startts != endts:
 			yield startts, endts, note, wheel, carry_newline
 			carry_newline = False
 
-def gen_output(fp):
+def gen_output(fp, notes):
 	cur_wheel = -10
-	for startts, endts, note, wheel, newline in filter_notes():
-		if newline:
-			fp.write("\n")
-		else:
-			fp.write(" ")
+	first = True
+	for startts, endts, note, wheel, newline in notes:
+		if not first:
+			if newline:
+				fp.write("\n")
+			else:
+				fp.write(" ")
+		first = False
 		if note is not None and abs(wheel - cur_wheel):
 			fp.write(f"$EE ${wheel:02X} ")
 			cur_wheel = wheel
@@ -83,7 +93,12 @@ def gen_output(fp):
 
 def main():
 	with open("out/09 - Trident's Stage/psg.txt", "w")  as fp:
-		gen_output(fp)
+		notes = gen_notes()
+		for channel in notes:
+			split = split_barlines(channel)
+			filtered = filter_notes(split)
+			gen_output(fp, filtered)
+			fp.write("\n\n")
 
 if __name__ == "__main__":
 	main()
